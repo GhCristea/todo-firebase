72ba0d243ea5e8c4b7ed1cf23f52d20bae165361
Gheorghita Cristea <gcristea73@gmail.com>
2021-07-12 16:14:44 +0100

    refactoring

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./styles.css";
+import "../public/styles.css";
 import { ProvideAuth } from "./firebase/auth";
 const mountNode = document.getElementById("app");
 ReactDOM.render(
