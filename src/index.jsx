f039179f5933ad9b1b758cb2b8c13e74ddc2a1c6
Gheorghita Cristea <gcristea73@gmail.com>
2021-08-07 12:24:53 +0100

    clear console log

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import { ProvideAuth } from "./firebase/auth";
 const mountNode = document.getElementById("app");
+console.log("inside index");
 ReactDOM.render(
   <ProvideAuth>
     <App />
