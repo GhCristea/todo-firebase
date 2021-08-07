cb9fb0d33da4abe8651248ebee2a6ecc7eff9d0e
Gheorghita Cristea <gcristea73@gmail.com>
2021-08-07 13:07:32 +0100

    fix merge

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "../public/styles.css";
+import "./styles.css";
 import { ProvideAuth } from "./firebase/auth";
 const mountNode = document.getElementById("app");
-console.log("inside index");
 ReactDOM.render(
   <ProvideAuth>
     <App />
