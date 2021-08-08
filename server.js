2ac29e9787b36748c0d4b03d9ee0f3ad45eaabb8
Gheorghita Cristea <gcristea73@gmail.com>
2021-08-08 12:57:29 +0100

    add catch all routes

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
 app.set("port", process.env.PORT || 8080);
 
 const server = app.listen(app.get("port"), function () {
